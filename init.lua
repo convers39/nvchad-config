e6b7074d74649fe32ddd6a750371d6fadca24aaa
Jerry <jerry31539@yahoo.com>
2023-08-29 12:52:53 +0900

    fix nvim-tree buffer restore with auto-session

--- init.lua
+++ init.lua
@@ -1,4 +1,16 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
+-- restore nvim-tree with auto-session
+autocmd({ "BufEnter" }, {
+  pattern = "NvimTree*",
+  callback = function()
+    local api = require "nvim-tree.api"
+    local view = require "nvim-tree.view"
+
+    if not view.is_visible() then
+      api.tree.open()
+    end
+  end,
+})
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
