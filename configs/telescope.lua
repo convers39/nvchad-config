235cb85bfbbc4ac79f37838dac29be43acef8125
Jerry <jerry31539@yahoo.com>
2023-12-07 10:12:45 +0900

    fix file ignore pattern in telescope

--- configs/telescope.lua
+++ configs/telescope.lua
@@ -37,7 +37,7 @@ local config = {
       preview_cutoff = 120,
     },
     file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = { "node_modules", "^.git$" },
+    file_ignore_patterns = { "node_modules", "^.git/.*$" },
     generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
     path_display = { "truncate" },
     winblend = 0,
